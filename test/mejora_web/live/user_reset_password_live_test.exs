ab02a09ea8e92c15369ec3d698ecaf2bf1b6df9f
Guillermo <guillermo.c.lobo@gmail.com>
2024-11-27 10:31:07 -0600

    fixed navigation cases

--- test/mejora_web/live/user_reset_password_live_test.exs
+++ test/mejora_web/live/user_reset_password_live_test.exs
@@ -93,7 +93,7 @@ defmodule MejoraWeb.UserResetPasswordLiveTest do
 
       {:ok, conn} =
         lv
-        |> element(~s|main a:fl-contains("Log in")|)
+        |> element(~s|a[href="/users/log_in"]|)
         |> render_click()
         |> follow_redirect(conn, ~p"/users/log_in")
 
@@ -108,7 +108,7 @@ defmodule MejoraWeb.UserResetPasswordLiveTest do
 
       {:ok, conn} =
         lv
-        |> element(~s|main a:fl-contains("Register")|)
+        |> element(~s|a[href="/users/register"]|)
         |> render_click()
         |> follow_redirect(conn, ~p"/users/register")
 

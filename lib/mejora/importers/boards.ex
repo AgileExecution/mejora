2bd0037d3b2522ce856f7c9b9c233993e3dc9fb0
thelastinuit <me@thelastinu.it>
2024-11-27 14:10:33 -0600

    fix: board and memberships importer.

--- lib/mejora/importers/boards.ex
+++ lib/mejora/importers/boards.ex
@@ -21,20 +21,20 @@ defmodule Mejora.Importers.Boards do
               transaction_id = Ecto.UUID.generate()
               loaded_president_transaction_id = "loaded-president-#{transaction_id}"
               loaded_secretary_transaction_id = "loaded-secretary-#{transaction_id}"
-              loaded_treasure_transaction_id = "loaded-treasure-#{transaction_id}"
+              loaded_treasurer_transaction_id = "loaded-treasurer-#{transaction_id}"
               board_id = "board-#{transaction_id}"
 
               multi_acc
-              |> load_user(record, loaded_president_transaction_id)
-              |> load_user(record, loaded_secretary_transaction_id)
-              |> load_user(record, loaded_treasure_transaction_id)
+              |> load_user(record, loaded_president_transaction_id, :president)
+              |> load_user(record, loaded_secretary_transaction_id, :secretary)
+              |> load_user(record, loaded_treasurer_transaction_id, :treasurer)
               |> Ecto.Multi.insert(
                 board_id,
                 Board.changeset(%Board{}, parse_board_record(record))
               )
               |> insert_board_membership(board_id, loaded_president_transaction_id, :president)
               |> insert_board_membership(board_id, loaded_secretary_transaction_id, :secretary)
-              |> insert_board_membership(board_id, loaded_treasure_transaction_id, :treasure)
+              |> insert_board_membership(board_id, loaded_treasurer_transaction_id, :treasurer)
 
             true ->
               multi_acc
@@ -56,20 +56,36 @@ defmodule Mejora.Importers.Boards do
   end
 
   defp parse_board_record(record) do
+    start_date =
+      if is_integer(Enum.at(record, 1)),
+        do: Timex.shift(~D[1900-01-01], days: Enum.at(record, 1)),
+        else: Timex.today()
+
+    end_date =
+      if is_integer(Enum.at(record, 2)),
+        do: Timex.shift(~D[1900-01-01], days: Enum.at(record, 2)),
+        else: Timex.today()
+
     %{
       name: Enum.at(record, 0),
-      start_date: Enum.at(record, 1),
-      end_date: Enum.at(record, 2),
-      status: Enum.at(record, 4),
+      start_date: start_date,
+      end_date: end_date,
+      status: parse_status(Enum.at(record, 13)),
       comments: Enum.at(record, 3)
     }
   end
 
-  defp load_user(multi_acc, _record, transaction_id) do
+  @user_matrix %{
+    president: %{name: 4, father_last_name: 5, mother_last_name: 6},
+    secretary: %{name: 7, father_last_name: 8, mother_last_name: 9},
+    treasurer: %{name: 10, father_last_name: 11, mother_last_name: 12}
+  }
+
+  defp load_user(multi_acc, record, transaction_id, role) do
     Ecto.Multi.run(multi_acc, transaction_id, fn _repo, _changes ->
-      name = ""
-      father_last_name = ""
-      mother_last_name = ""
+      name = Enum.at(record, @user_matrix[role][:name])
+      father_last_name = Enum.at(record, @user_matrix[role][:father_last_name])
+      mother_last_name = Enum.at(record, @user_matrix[role][:mother_last_name])
 
       case Repo.get_by(User,
              name: name,
@@ -94,4 +110,15 @@ defmodule Mejora.Importers.Boards do
       })
     end)
   end
+
+  def valid_date?(string, format \\ "{YYYY}-{0M}-{0D}") do
+    case Timex.parse(string, format) do
+      {:ok, _date} -> true
+      {:error, _reason} -> false
+    end
+  end
+
+  defp parse_status("Inactiva"), do: :inactive
+  defp parse_status("Activa"), do: :active
+  defp parse_status(_), do: :inactive
 end

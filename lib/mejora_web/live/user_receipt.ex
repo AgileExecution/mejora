c1ce815097d7239007de4b3511a6c8371de27bfc
GaussNajera245 <60408037+GaussNajera245@users.noreply.github.com>
2025-03-04 16:06:56 -0600

    make a page for each payment notice

--- lib/mejora_web/live/user_receipt.ex
+++ lib/mejora_web/live/user_receipt.ex
@@ -1,15 +1,27 @@
 defmodule MejoraWeb.Live.UserReceipt do
   use MejoraWeb, :live_view
+  import Ecto.Query
 
-  def mount(_params, _session, socket) do
+  alias Mejora.Repo
+  alias Mejora.Neighborhoods.Quota
+  alias Mejora.Transactions.PaymentNotice
+
+  def mount(params, _session, socket) do
+    payment_id = params["id"] |> Integer.parse() |> elem(0)
+    payment =
+      PaymentNotice
+      |> where(id: ^payment_id)
+      |> Repo.one()
+
+    payed = 0
     quota = %{
-      id: 38,
+      id: payment.id,
       concept: "General",
-      date_start: "01-07-2025",
-      date_end: "10-07-2025",
-      incoming: 750,
-      payed: 0,
-      pending: 750
+      due_date: payment.due_date,
+      incoming: payment.total,
+      payed: payed,
+      pending: to_num(payment.total) - payed,
+      comments: payment.comments || "-"
     }
 
     {:ok, assign(socket, :quota, quota)}
@@ -19,6 +31,10 @@ defmodule MejoraWeb.Live.UserReceipt do
     list
     |> Enum.reduce(0, fn arr, acc -> arr.pending + acc end)
     |> Number.Currency.number_to_currency()
+  end
+
+  def to_num(value) do
+    value |> Decimal.to_integer()
   end
 
   def currency(num) do
